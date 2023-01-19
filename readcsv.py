6594e1f09dc39ea6d70cf1ceb4d64e758881bb10
migueldi <migueldi.tech@gmail.com>
2023-01-19 16:51:40 -0300

    Lectura de csv añadida

---
+++ readcsv.py
@@ -0,0 +1,17 @@
+import csv
+
+def readCsv(path):
+    with open (path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        header = next(reader)  #iterable
+        orders = []
+        for row in reader:
+            iterable = zip(header, row)
+            order = {key: value for key, value in iterable}
+            orders.append(order)
+    return orders
+
+if __name__ == '__main__':
+  orders = readCsv('./app/superstore.csv')
+  print(orders[2939])
+
